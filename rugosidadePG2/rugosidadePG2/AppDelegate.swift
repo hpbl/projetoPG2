8b694299f1639f98bffc6686237746c95fda0115
Hilton Pintor <hpbl@cin.ufpe.br>
2016-12-08 21:37:42 -0300

    Adicionando projeto do XCode

---
+++ rugosidadePG2/rugosidadePG2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  rugosidadePG2
+//
+//  Created by Hilton Pintor Bezerra Leite on 08/12/16.
+//  Copyright © 2016 Chien&Pintor&Melo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ rugosidadePG2/rugosidadePG2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  rugosidadePG2
+//
+//  Created by Hilton Pintor Bezerra Leite on 08/12/16.
+//  Copyright © 2016 Chien&Pintor&Melo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
