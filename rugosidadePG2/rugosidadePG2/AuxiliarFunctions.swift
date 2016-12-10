c38a5873092c0b55478be2d4a7f782bd00df6fdb
fanny <fc2@cin.ufpe.br>
2016-12-10 01:06:46 -0300

    adicionando novas formulas auxiliares

--- rugosidadePG2/rugosidadePG2/AuxiliarFunctions.swift
+++ rugosidadePG2/rugosidadePG2/AuxiliarFunctions.swift
@@ -25,5 +25,33 @@ func read(from file: String, type: String) -> [String]? {
 //MARK: - Vector formulas
 
 func innerProduct(u: Point, v: Point) -> Double {
-    return (u.x * v.x) + (u.y + v.y) + (u.z! + v.z!)
+    return (u.x * v.x) + (u.y * v.y) + (u.z! * v.z!)
 }
+
+func vectorByScalar(a: Double , v: Point) -> Point {
+    return Point(x: a*(v.x), y: a*(v.y), z: a*(v.z)!)
+    
+}
+
+func projection(u: Point, v: Point) ->  Point {
+    return vectorByScalar(a: innerProduct(u: v, v: u)/innerProduct(u: u, v: u) , v: u)
+}
+
+func vectorSubtraction(u: Point, v: Point) -> Point {
+    return Point(x: u.x - v.x , y: u.y - v.y , z: u.z! - v.z!)
+    
+}
+
+func orthogonalization(N: Point, V: Point) -> Point {
+    return vectorSubtraction(u: V, v: projection(u: N, v: V))
+}
+
+func crossProduct(u: Point , v: Point) -> Point {
+    let x = (u.y*v.z!) - (v.z!*v.y)
+    let y = (u.z!*v.x) - (u.x*v.z!)
+    let z = (u.x*v.y) - (u.y*v.x)
+    
+    return Point(x: x, y: y, z: z)
+}
+
+
